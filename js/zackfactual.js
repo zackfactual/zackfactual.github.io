f587325ba0746bacff28740942bd2ee703487030
Zachary Velcoff <zackfactual@gmail.com>
2018-02-07 15:01:18 -0800

    add c#, update skills and year

---
+++ js/zackfactual.js
@@ -0,0 +1,27 @@
+$(document).ready(function() { 
+		
+    /* close navbar when clicks navbar link */
+    $( ".nav-link" ).click(function() {
+        $( ".navbar-toggler" ).click();
+    });
+
+    /* close navbar when user clicks away from navbar */
+    $(document).click(function(e) {
+        if (!$(e.target).is('a')) {
+            $('.collapse').collapse('hide');	    
+        }
+    });
+
+    /* set carousel to rotate every 3 seconds */
+    $('.carousel').carousel({
+        interval: 3000
+    });
+
+    /* set carousel to cease rotating on hover, resume rotating when off hover */
+    $('carousel').hover(function() {
+        $(this).carousel('pause');
+    }, function() {
+        $(this).carousel('cycle');
+    });
+		
+});
