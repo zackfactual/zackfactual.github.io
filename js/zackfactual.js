df3662b7420ff4d696eac567c4d8dd46a7c80a4f
Zachary Velcoff <zackfactual@gmail.com>
2018-09-13 14:57:30 -0700

    test border-radius removal in ie/edge

--- js/zackfactual.js
+++ js/zackfactual.js
@@ -19,5 +19,15 @@ $(document).ready(function() {
     // auto-update copyright
     var year = (new Date).getFullYear();
     $("#copy").append("&copy; " + year + " — Zachary Velcoff");
+
+    var isIE = /*@cc_on!@*/false || !!document.documentMode, // Internet Explorer 6-11
+    isEdge = !isIE && !!window.StyleMedia; // Edge 20+
+
+    // Check if Internet Explorer 6-11 OR Edge 20+
+    if(isIE || isEdge) {
+        document.getElementsByClassName('img-circle').style = "border-radius: 0%;"
+    }
 });
+
+
 
